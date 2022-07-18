c689bd1c42dcdc5c9597ee359de4036b7f833a72
Bharat <bharat.joshi.bly@gmail.com>
2022-07-18 22:42:58 +0530

    Updates

---
+++ precache-manifest.fdfa29848a2199ec5b11ead50568704a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f05a11420293279eb13e26fbfe1cd3c",
+    "url": "/box-office-app/index.html"
+  },
+  {
+    "revision": "43e1207ed4a5015b163b",
+    "url": "/box-office-app/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "f27014f3df79d3ddbe19",
+    "url": "/box-office-app/static/js/2.b9ad38cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office-app/static/js/2.b9ad38cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e1207ed4a5015b163b",
+    "url": "/box-office-app/static/js/main.afa5d683.chunk.js"
+  },
+  {
+    "revision": "e84a49faa35fea3d975c",
+    "url": "/box-office-app/static/js/runtime-main.27edc3ee.js"
+  }
+]);
